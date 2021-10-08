26ed7c80b8bf7773babc1a76fd7072864c860b37
2Bray <17190440@bsi.ac.id>
2021-10-08 08:36:10 +0700

    Step-2
    Adding Movement

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    private float speed = 0.0001f;
+    Rigidbody2D playerRb;
+
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        playerRb.velocity = Vector2.right;
+    }
+}
