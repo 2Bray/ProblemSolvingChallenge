ddca0d55afc3ae07376e0822b5af4d1f76dffa92
2Bray <17190440@bsi.ac.id>
2021-10-08 09:55:39 +0700

    Step-4
    Adding Controll Movement

---
+++ Assets/Scripts/ControlPlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayerScript : MonoBehaviour
+{
+    private float speed = 0.01f;
+    private Rigidbody2D playerRb;
+    private bool hit = false;
+
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerRb.sharedMaterial.bounciness = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) playerRb.velocity = Vector2.up * 5;
+        if (Input.GetKey(KeyCode.D)) playerRb.velocity = Vector2.right * 5;
+        if (Input.GetKey(KeyCode.S)) playerRb.velocity = Vector2.down * 5;
+        if (Input.GetKey(KeyCode.A)) playerRb.velocity = Vector2.left * 5;
+    }
+}
