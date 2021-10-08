aaca840d1261c2ee281cda916865e8efc3cb5058
2Bray <17190440@bsi.ac.id>
2021-10-08 11:10:12 +0700

    Step-6
    Adding Food (Random Number Of Instantiate & Random Position)

---
+++ Assets/Scripts/FoodControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodControllerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject food;
+    [SerializeField] private Transform[] walls;
+
+    private void Start()
+    {
+        for (int i = Random.Range(0, 10); i>= 0; i--)
+        {
+            Debug.Log(i + 1);
+
+            float posX = Random.Range(walls[3].position.x + 1, walls[1].position.x - 1);
+            float posY = Random.Range(walls[2].position.y + 1, walls[0].position.y - 1);
+            Vector3 foodPosition = new Vector3(posX, posY, 0);
+            Instantiate(food, foodPosition, Quaternion.identity);
+        }
+    }
+}
