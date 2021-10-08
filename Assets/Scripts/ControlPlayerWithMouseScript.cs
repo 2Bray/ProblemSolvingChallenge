3cfa14a1e3c360489ddb18bc7da6fab02fbd7040
2Bray <17190440@bsi.ac.id>
2021-10-08 10:26:34 +0700

    Step-5
    Cotroll Player With Mouse

---
+++ Assets/Scripts/ControlPlayerWithMouseScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayerWithMouseScript : MonoBehaviour
+{
+    private float speed = 1f;
+    private Rigidbody2D playerRb;
+
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerRb.sharedMaterial.bounciness = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z;
+        playerRb.AddForce( mousePosition * speed * Time.deltaTime, ForceMode2D.Impulse);
+    }
+}
