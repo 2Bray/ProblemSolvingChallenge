1eedc2efc5f839fbb317020fb92b92ef2cfd161c
2Bray <17190440@bsi.ac.id>
2021-10-08 09:06:50 +0700

    Step-3
    Adding Block

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    private float speed = 0.0001f;
+    private Rigidbody2D playerRb;
+    private bool hit = false;
+
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!hit) playerRb.velocity = Vector2.right * 10;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) => hit = true;
+    
+}
